4e4cab248d287855e10aa8b009420113d43f105b
Aldhair Said Mamani Ramos <51174683+NOTCompile@users.noreply.github.com>
2025-09-09 18:23:17 -0500

    Comentario sobre código inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,7 +3,8 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        System.out.println("Esta instrucción es inservible");
+        //SIMULACION: Se comento la línea de codigo que no aporta al proyecto - se elimina
+        //System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
