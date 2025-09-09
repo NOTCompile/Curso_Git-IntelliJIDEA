110d3ed0b8d3cf5c65e7c76d8fa64aa3a69f6d5c
NOTCompile <notcompile@gmail.com>
2025-09-09 18:30:00 -0500

    Se retiro la línea de código inservible.

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,8 +3,6 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        //SIMULACION: Se comento la línea de codigo que no aporta al proyecto - se elimina
-        //System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
