87d2580fba5b7da29d3c4ca2d5cc7229fae7cbe1
NOTCompile <notcompile@gmail.com>
2025-09-09 18:11:31 -0500

    Se creo la clase Persona

--- src/com/acme/Hola_Mundo.java
+++ src/com/acme/Hola_Mundo.java
@@ -4,7 +4,9 @@ public class Hola_Mundo {
     public static void main(String[] args) {
         Mensaje msg = new Mensaje();
         String nombre = msg.solicitaDatos("Escribe tu nombre: ");
-        msg.saludar(nombre);
+        String apellidos = msg.solicitaDatos("Escribe tu apellido: ");
+        Persona p1 = new Persona(nombre, apellidos);
+        msg.saludar(p1);
     }
 
 }
